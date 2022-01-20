19780a903fbc78a22f48fa671790a50a75714885
ybuchi <yabuch_ch@hotmail.com>
2022-01-20 13:21:07 -0500

    Added notes and code

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!")
+}
+sayHello();
+
+function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+function sayHelloToLiz() {
+    console.log("Hello, Liz!")
+}
+
+function sayHelloToSamip() {
+    console.log("Hello, Samip!")
+}
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+// Parameters allow our functions to be more flexible
+// Parameter names only have meaning to us so they can be anything
+
+function sayHelloTo(firstName) {
+    console.log(`Hello ${firstName}!`);
+    return `Hello ${firstName}!`
+}
+
+sayHelloTo("Guadalupe");
+sayHelloTo("Samip");
+sayHelloTo("Liz");
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+    return `${greeting}, ${firstName}!`;
+  }
+
+  say("Julio", "hello");  
+
+function say(greeting, firstName) {
+console.log("firstName: ", firstName);
+console.log("greeting: ", greeting);
+console.log(`${greeting}, ${firstName}!`);
+}
+
+// Return values in JavaScript
+function add(x,y) {
+    return x + y;
+}
+
+console.log(add(3020,2132));
+
+//Practice writing functinos
+
+function stringChange(string){
+    console.log(string.split(", "));
+    return string.split();
+}
+stringChange("Bread, Butter, Coffee, Cakes");
